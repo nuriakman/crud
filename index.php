50847aac830fd56fb702556f2efa4141b0ea4349
Nuri Akman <nuriakman@gmail.com>
2019-02-11 14:30:45 +0300

    Listele tamamlandı

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+  ## Veritabanına bağlantı kuralım...
+  ## Veritabanına bağlantı kuralım...
+  $host     = "localhost";
+  $user     = "root";
+  $password = "root";
+  $database = "kis_kampi_ornekleri";
+  $cnnMySQL = mysqli_connect( $host, $user, $password, $database );
+  if( mysqli_connect_error() ) die("Veritabanına bağlanılamadı...");
+  $temp = mysqli_query($cnnMySQL, "set names 'utf8'");
+?>
 <html>
 <head>
   <meta charset="utf-8" />
@@ -5,5 +16,41 @@
 </head>
 <body>
   <h1>Telefon Rehberi</h1>
+<?php
+## Veritabanından kayıt çekme ve TABLE ile listeleme örneği
+## Veritabanından kayıt çekme ve TABLE ile listeleme örneği
+
+$SQL = "SELECT * FROM telefonrehberi";
+$rows = mysqli_query($cnnMySQL, $SQL);
+$RowCount = mysqli_num_rows($rows);
+if($RowCount == 0) { // Kayıt yok...
+  echo "Rehberde Kayıt bulunamadı...";
+} else { // Kayıt var
+  // Tablo başlığını yazdıralım
+  echo "<table class='table table-hover' border=1 cellpadding=10 cellspacing=0>
+          <tr>
+              <th>SıraNo</th>
+              <th>Adı Soyadı</th>
+              <th>Telefonu</th>
+           </tr>";
+  $c=0;
+  while($row = mysqli_fetch_assoc($rows)) {
+    extract($row); // "Key" adında değişkenler oluştur :)
+    $c++;
+    // Tablo başlığını yazdıralım
+    echo "<tr>
+            <td>$c</td>
+            <td>$adisoyadi</td>
+            <td>$telefonu</td>
+         </tr>";
+  } // while
+  echo "</table>";
+} // Kayıt var
+?>
+
+<p>
+  <a href="">Yeni kayıt ekle...</a>
+</p>
+
 </body>
 </html>
