ab463abb7e37c23df39e0b305d4ad2bd034246a0
Nuri Akman <nuriakman@gmail.com>
2019-02-11 15:32:40 +0300

    Kayıt güncelleme tamamlandı

--- crud.update.php
+++ crud.update.php
@@ -1,3 +1,39 @@
+<?php
+  ## Veritabanına bağlantı kuralım...
+  ## Veritabanına bağlantı kuralım...
+  $host     = "localhost";
+  $user     = "root";
+  $password = "root";
+  $database = "kis_kampi_ornekleri";
+  $cnnMySQL = mysqli_connect( $host, $user, $password, $database );
+  if( mysqli_connect_error() ) die("Veritabanına bağlanılamadı...");
+  $temp = mysqli_query($cnnMySQL, "set names 'utf8'");
+
+  if(isset($_POST["adi"])) {
+    ## Veritabanına kayıt ekleme
+    ## Veritabanına kayıt ekleme
+    // SQL içine konulacak değişkenlere MUTLAKA bu işlem uygulanmalıdır. Bunun sebebi GÜVENLİK'tir.
+    $adi = mysqli_real_escape_string($cnnMySQL, $_POST["adi"]);
+    $tel = mysqli_real_escape_string($cnnMySQL, $_POST["tel"]);
+    $KAYITNO = mysqli_real_escape_string($cnnMySQL, $_POST["id"]);
+
+    $SQL = "UPDATE telefonrehberi SET
+                adisoyadi = '$adi',
+                telefonu  = '$tel'
+            WHERE id = '$KAYITNO'       ";
+    $rows = mysqli_query($cnnMySQL, $SQL);
+    echo "<p>Kayıt güncellendi...</p>";
+    echo "<a href='index.php'>Ana Sayfa....</a>";
+    die();
+  }
+
+
+  $KAYITNO = $_GET["kayitno"];
+  $SQL  = "SELECT * FROM telefonrehberi WHERE id='$KAYITNO'  ";
+  $rows = mysqli_query($cnnMySQL, $SQL);
+  $row  = mysqli_fetch_assoc($rows);
+
+?>
 <html>
 <head>
   <meta charset="utf-8" />
@@ -8,10 +44,11 @@
 
    <form action="" method="post">
 
-    <p> Adı Soyadı: <input type="text" name="adi" /> </p>
-    <p> Telefonu:   <input type="text" name="tel" /> </p>
+    <p> Adı Soyadı: <input type="text" name="adi" value="<?=$row["adisoyadi"]?>" /> </p>
+    <p> Telefonu:   <input type="text" name="tel" value="<?=$row["telefonu"]?>"/> </p>
     <p>
         <input type="submit" value="Güncelle" />
+        <input type="hidden" name="id" value="<?=$row["id"]?>"/>
         <a href="index.php">Vazgeç...</a>
     </p>
 
